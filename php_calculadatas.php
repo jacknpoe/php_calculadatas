ecef870841c474f96b7f43da5fc61cbcb89bbef4
jacknpoe <jacknpoe@yahoo.com>
2023-04-21 21:41:42 -0300

    Add files via upload

---
+++ php_calculadatas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<title>Cálculo da diferença entre duas datas</title>
+ 		<link rel="stylesheet" href="php_calculadatas.css"/>
+		<link rel="icon" type="image/png" href="php_calculadatas.png"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<?php
+			header( "Content-Type: text/html; charset=ISO-8859-1", true);
+
+			$resultado = '';
+			$datainicial = 
+
+			if( isset( $_POST[ 'calcular']))
+			{
+				// mágica
+			}
+		?>
+		<h1>Cálculo da diferença entre duas datas<br></h1>
+
+		<form action="php_calculadatas.php" method="POST" style="border: 0px">
+			<p>Data Inicial: <input type="date" name="datainicial" value="<?php echo $datainicial; ?>"></p>
+			<p>Data Final: <input type="date" name="datafinal" value="<?php echo $datafinal; ?>"></p>
+			<p><input type="submit" name="calcular" value="Calcular"></p>
+		</form>
+
+		<br><p>Resultado: <?php echo $resultado; ?>%</p><br><br>
+		<p><a href="https://github.com/jacknpoe/php_calculadatas">Repositório no GitHub</a></p><br><br>
+		<form action="index.html" method="POST" style="border: 0px">
+			<p><input type="submit" name="voltar" value="Voltar"></p>
+		</form>
+	</body>
+</html>
